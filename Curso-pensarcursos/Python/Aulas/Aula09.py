1ef228a122fabfc68a6249fe42e12727ce7382ae
GledsonVC <gledsonvc@gmail.com>
2025-02-04 18:14:29 -0300

    Feito Aula09.py

---
+++ Curso-pensarcursos/Python/Aulas/Aula09.py
@@ -0,0 +1,10 @@
+# Explicou como fazer para criar um arquivo ou ler o mesmo
+arquivo = open(+"Hugo.txt", 'w')
+
+"""
+w - write   -Escrever
+r - read    -Ler
+a - append  -Extender
+"""
+
+arquivo.close()
