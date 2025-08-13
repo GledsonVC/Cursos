a8e50fba0d584590d7ee73cc47016c599e2352d8
GledsonVC <gledsonvc@gmail.com>
2025-08-13 14:54:22 -0300

    Teste feito com função

--- CursoIntensivoDePython/python_work/teste.py
+++ CursoIntensivoDePython/python_work/teste.py
@@ -1,2 +1,17 @@
-import random
-print(random.randint(0, 1000))
+def funcaoTupla(*tupla_variavel_lista_dicionario):
+    return tupla_variavel_lista_dicionario
+
+
+def funcaoDicionario(**dicionario_tupla_variavel_lista):
+    return dicionario_tupla_variavel_lista
+
+
+tupla = funcaoTupla('valor1', 'valor2') 
+
+dicionario = funcaoDicionario(
+    indice1 = 'valor1', 
+    indice2 = 'valor2',
+)
+
+print(tupla)
+print(dicionario)
